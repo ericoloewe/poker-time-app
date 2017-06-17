f95abfd5f23f315e82234568a1943c198b19730a
Érico de Souza Loewe <ed-lowe@hotmail.com>
2017-06-17 17:05:13 -0300

    feat(lang): add lang config.

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,8 @@
 /**
  * Configs
  */
+import { LanguageBuilder } from "./lang/index";
 
 export { ENDPOINTS, ROUTES, URLS, COLORS, STYLES, VALUES } from "./constants/index";
 export { Routes } from "./routes/index";
+export { LanguageBuilder, LanguageBuilder as LB }
---
+++ src/configs/lang/pt-br/index.js
@@ -0,0 +1,13 @@
+import { ABOUT, BANKHALL, HOME, SETTINGS, TRY_LUCK } from "./containers/index";
+
+const CONTAINERS = {
+    ABOUT,
+    BANKHALL,
+    HOME,
+    TRY_LUCK,
+    SETTINGS
+};
+
+export {
+    CONTAINERS
+}
